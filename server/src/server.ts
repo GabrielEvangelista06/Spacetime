bf8705802ab09cbc2b053f5885a3194d06b0319d
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-05-16 14:15:22 -0300

    Setup base do backend

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client';
+import fastify from 'fastify';
+
+const app = fastify();
+const prisma = new PrismaClient();
+
+app
+  .listen({
+    port: 3333
+  })
+  .then(() => {
+    console.log('🚀 HTTP server listening on http://localhost:3333');
+  });
