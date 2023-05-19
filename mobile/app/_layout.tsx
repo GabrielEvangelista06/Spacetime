1df53ba329fd513624230dd6dee9744ef23bd019
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-05-19 11:33:51 -0300

    Personalizando rota inicial do app

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -5,9 +5,10 @@ import {
   useFonts,
 } from '@expo-google-fonts/roboto'
 import { SplashScreen, Stack } from 'expo-router'
+import * as SecureStore from 'expo-secure-store'
 import { StatusBar } from 'expo-status-bar'
 import { styled } from 'nativewind'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ImageBackground } from 'react-native'
 import blurBg from '../src/assets/bg-blur.png'
 import Stripes from '../src/assets/stripes.svg'
@@ -15,11 +16,21 @@ import Stripes from '../src/assets/stripes.svg'
 const StyledStripes = styled(Stripes)
 
 export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<
+    null | boolean
+  >(null)
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) => {
+      setIsUserAuthenticated(!!token)
+    })
+  }, [])
 
   if (!hasLoadedFonts) {
     return <SplashScreen />
@@ -39,7 +50,10 @@ export default function Layout() {
           headerShown: false,
           contentStyle: { backgroundColor: 'trasnparent' },
         }}
-      />
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="memories" />
+      </Stack>
     </ImageBackground>
   )
 }
