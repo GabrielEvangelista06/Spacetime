0d79db9ba9305e217f4e85fc45904bd87843e401
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-05-19 12:33:05 -0300

    Realizando upload de imagem e servindo arquivos estáticos

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,33 @@
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import 'dotenv/config'
 import fastify from 'fastify'
+import { resolve } from 'path'
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { upload } from './routes/upload'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
 })
+
 app.register(jwt, {
   secret: 'spacetime',
 })
+
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(upload)
 
 app
   .listen({
