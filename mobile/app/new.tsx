6f08887737da5be16ce98fae2cf3c54684b59ec1
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-05-23 16:20:31 -0300

    Implementando a criação de uma nova memória

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -1,6 +1,7 @@
 import Icon from '@expo/vector-icons/Feather'
 import * as ImagePicker from 'expo-image-picker'
-import { Link } from 'expo-router'
+import { Link, useRouter } from 'expo-router'
+import * as SecureStore from 'expo-secure-store'
 import React, { useState } from 'react'
 import {
   Image,
@@ -13,11 +14,14 @@ import {
 } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Logo from '../src/assets/spacetime-logo.svg'
+import { api } from '../src/lib/api'
 
 export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets()
+  const router = useRouter()
 
   const [preview, setPreview] = useState<string | null>(null)
+
   const [content, setContent] = useState('')
   const [isPublic, setIsPublic] = useState(false)
 
@@ -36,7 +40,45 @@ export default function NewMemory() {
     }
   }
 
-  async function handleCreateMemory() {}
+  async function handleCreateMemory() {
+    const token = await SecureStore.getItemAsync('token')
+
+    let coverUrl = ''
+
+    if (preview) {
+      const uploadFormData = new FormData()
+
+      uploadFormData.append('file', {
+        uri: preview,
+        name: 'image.jpg',
+        type: 'image/jpeg',
+      } as any)
+
+      const uploadResponse = await api.post('/upload', uploadFormData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+
+      coverUrl = uploadResponse.data.fileUrl
+    }
+
+    await api.post(
+      '/memories',
+      {
+        content,
+        isPublic,
+        coverUrl,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token} `,
+        },
+      },
+    )
+
+    router.push('/memories')
+  }
 
   return (
     <ScrollView
@@ -90,6 +132,7 @@ export default function NewMemory() {
           multiline
           value={content}
           onChangeText={setContent}
+          textAlignVertical="top"
           className="p-0 font-body text-lg text-gray-50"
           placeholderTextColor="#56565a"
           placeholder="Fique livre para adicionar foto, vídeos e relatos sobre essa experiência que você quer lembrar para sempre"
